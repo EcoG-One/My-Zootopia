87e50954347b147ffcdbc21a6390436d772bdd4b
Georgios Oikonomou <ecog@outlook.de>
2025-01-25 07:40:01 +0100

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -6,12 +6,25 @@ def load_data(file_path):
     return json.load(handle)
 
 
-animals_data = load_data('animals_data.json')
-for i in range(len(animals_data)):
-    try:
-        print("Name:", animals_data[i]["name"])
-        print("Diet:", animals_data[i]["characteristics"]["diet"])
-        print("Location:", animals_data[i]["locations"][0])
-        print("Type:", animals_data[i]["characteristics"]["type"])
-    except (KeyError, IndexError):
-        pass
+def read_data():
+    animals_data = load_data('animals_data.json')
+    output = ''
+    for i in range(len(animals_data)):
+        try:
+            output += "Name: " + animals_data[i]["name"] + "\n"
+            output += "Diet:" + animals_data[i]["characteristics"]["diet"] + "\n"
+            output += "Location:" + animals_data[i]["locations"][0] + "\n"
+            output += "Type:" + animals_data[i]["characteristics"]["type"] + "\n"
+        except (KeyError, IndexError):
+            pass
+    return output
+
+
+def read_html():
+    with open("animals_template.html", "r") as html_template:
+        return html_template.read()
+
+
+new_html = read_html().replace("__REPLACE_ANIMALS_INFO__", read_data())
+with open("animals.html", "w") as new_html_file:
+    new_html_file.write(new_html)
