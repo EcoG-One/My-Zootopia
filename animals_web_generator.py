4f1fa9c78894f07a73f33fd5eb0297aeb8d7a755
Georgios Oikonomou <ecog@outlook.de>
2025-02-03 08:09:12 +0100

    Milestone 3

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -4,20 +4,15 @@ import requests
 
 def load_data(animal):
   '''
-  Loads a JSON file
-  :param file_path: the path to the JSON file
+  Loads a JSON file about animals from api-ninjas.com
+  :param animal: animal family to fetch
   :return: the content of the JSON file as a list of dictionaries
-
-      with open(file_path, "r") as handle:
-        data = handle.read()
-  except IOError as e:
-      print(f'WARNING! {e}. Exiting...')
-      exit()
-  return json.loads(data)'''
+'''
   HEADER = {'X-Api-Key': 'G9QyJcWfkTyDv5FL9jdR6Q==pRFfUbsYWJ4ldFmm'}
   PARAM = {'name': animal}
   API_URL = 'https://api.api-ninjas.com/v1/animals'
-  result = None
+
+  result = []
   response = requests.get(API_URL, headers=HEADER, params=PARAM)
   if response.status_code == requests.codes.ok:
       result = response.json()
@@ -60,27 +55,11 @@ def serialize_animal(animal_obj):
     return output
 
 
-def skin_type_list(animals_data):
-    '''
-    Creates a list of available animal skin types
-    :param animals_data: List of Dictionaries with all the animals
-        and their properties
-    :return: a list of available animal skin types
-    '''
-    skin_list =[]
-    for animal_data in animals_data:
-        if animal_data["characteristics"]["skin_type"] not in skin_list:
-            skin_list.append(animal_data["characteristics"]["skin_type"])
-    return skin_list
-
-
 def read_data(animals_data):
     '''
     Iterates through the animals
     :param animals_data: List of Dictionaries with all the animals and
         their properties
-    :param skin_choice: Users choice of animal skin to generate the website
-           only for the animals with the selected skin_choice
     :return: all selected animal data as HTML
     '''
     output = ''
@@ -104,26 +83,11 @@ def read_html():
 
 def main():
     '''
-    Main function: Displays to the user a list of the available
-        skin_type values from the JSON file and asks for the user to
-        enter a skin_type from the displayed list, and then,
-        based on the "animals_template.html", generates the webpage
-        "animals.html" for the animals with the selected skin_type,
-        or all if user chose all.
+    Main function: asks for the user to enter an animal family,
+    and then, based on the "animals_template.html",
+    generates the webpage "animals.html" for the animals of that family.
     :return: HTML page
-    animals_data = load_data('animals_data.json')
-    print("Available skin types are: ")
-    for skin in skin_type_list(animals_data):
-        print(skin)
-    while True:
-        choice = input("Please enter a skin type or "
-                       "'All' for all skin types: ").capitalize()
-        if choice == "'all'": #In case user inputs 'All' with quotation
-            choice = "All"
-        if choice in skin_type_list(animals_data) or choice == "All":
-            break
-        else:
-            print("Wrong input.")'''
+    '''
     choice = input("Enter a name of an animal: ").capitalize()
     animals_data  = load_data(choice)
     if not animals_data:
